96a69c2a70212c50105ee9a02e08493830816e32
Valerie <sprudelwasser100@gmail.com>
2026-06-06 18:56:11 +0200

    Everest Full Game

--- microgames/microgames.gd
+++ microgames/microgames.gd
@@ -5,7 +5,8 @@ class_name MicroGames
 const scenes: Dictionary[GpnVersion, Array] = {
 	GpnVersion.Gpn24: [
 		preload("res://microgames/flopper/main.tscn"),
-    preload("res://microgames/music-memory/music_memory_main.tscn")
+		preload("res://microgames/music-memory/music_memory_main.tscn")
+		preload("res://microgames/everest/everest.tscn")
 	],
 	GpnVersion.Gpn23: [
 		preload("res://microgames/2048/main.tscn"),
